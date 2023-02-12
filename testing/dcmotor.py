425f6b735d3a174f9866756d532998decf128e37
Joe <kuttehj@etown.edu>
2023-02-12 11:07:57 -0500

    adding code for 4 dc motors and more functions

--- testing/dcmotor.py
+++ testing/dcmotor.py
@@ -1,25 +1,101 @@
+#Imports
 import RPi.GPIO as GPIO
 import time
 import yolov5   
 
+#Variables
+#Enable pins can turn the motors on/off and control speed via PWM
+enableFront = 16
+enableRear = 16
+
+#Input pins control direction of the motors
+#If both inputs are LO the motor is off, both are HI and motor is off
+#input1 is HI and input2 is LO motor is spinning forward
+#input1 is LO and input2 is HI motor is spinning backward
+MOTOR1_input1 = 20
+MOTOR1_input2 = 21
+
+MOTOR2_input1 = 20
+MOTOR2_input2 = 21
+
+MOTOR3_input1 = 20
+MOTOR3_input2 = 21
+
+MOTOR4_input1 = 20
+MOTOR4_input2 = 21
+
+
+#This function control the speed of the DC motor. Speed must be between 0-100
+def speed_control(targetPWM,speed):
+    targetPWM.ChangeDutyCycle(speed)
+
+#forward
+def forward(MOTOR1_input1,MOTOR1_input2,MOTOR2_input1,MOTOR2_input2,MOTOR3_input1,MOTOR3_input2,MOTOR4_input1,MOTOR4_input2):
+    GPIO.output(MOTOR1_input1,GPIO.HIGH)
+    GPIO.output(MOTOR1_input2,GPIO.LOW)
+    
+    GPIO.output(MOTOR2_input1,GPIO.HIGH)
+    GPIO.output(MOTOR2_input2,GPIO.LOW)
+
+    GPIO.output(MOTOR3_input1,GPIO.HIGH)
+    GPIO.output(MOTOR3_input2,GPIO.LOW)
+
+    GPIO.output(MOTOR4_input1,GPIO.HIGH)
+    GPIO.output(MOTOR4_input2,GPIO.LOW)
+
+#reverse 
+def reverse(MOTOR1_input1,MOTOR1_input2,MOTOR2_input1,MOTOR2_input2,MOTOR3_input1,MOTOR3_input2,MOTOR4_input1,MOTOR4_input2):
+    GPIO.output(MOTOR1_input1,GPIO.LOW)
+    GPIO.output(MOTOR1_input2,GPIO.HIGH)
+    
+    GPIO.output(MOTOR2_input1,GPIO.LOW)
+    GPIO.output(MOTOR2_input2,GPIO.HIGH)
+
+    GPIO.output(MOTOR3_input1,GPIO.LOW)
+    GPIO.output(MOTOR3_input2,GPIO.HIGH)
+
+    GPIO.output(MOTOR4_input1,GPIO.LOW)
+    GPIO.output(MOTOR4_input2,GPIO.HIGH)
+
+#left
+def left():
+
+
+#right
+def right():
+
+#stop
+def off(MOTOR1_input1,MOTOR1_input2,MOTOR2_input1,MOTOR2_input2,MOTOR3_input1,MOTOR3_input2,MOTOR4_input1,MOTOR4_input2):
+    GPIO.output(MOTOR1_input1,GPIO.LOW)
+    GPIO.output(MOTOR1_input2,GPIO.LOW)
+    GPIO.output(MOTOR2_input1,GPIO.LOW)
+    GPIO.output(MOTOR2_input2,GPIO.LOW)
+    GPIO.output(MOTOR3_input1,GPIO.LOW)
+    GPIO.output(MOTOR3_input2,GPIO.LOW)
+    GPIO.output(MOTOR4_input1,GPIO.LOW)
+    GPIO.output(MOTOR4_input2,GPIO.LOW)
+
+#Setting how board GPIOs are numbered
 GPIO.setmode(GPIO.BCM)
-enable = 16
-input1 = 20
-input2 = 21
-GPIO.setup(enable,GPIO.OUT)
-GPIO.setup(input1,GPIO.OUT)
-GPIO.setup(input2,GPIO.OUT)
 
-# GPIO.output(enable,GPIO.HIGH)
-enablePWM = GPIO.PWM(enable,50)
-enablePWM.start(0)
+#Setting GPIO to be input or output
+GPIO.setup(enableFront,GPIO.OUT)
+GPIO.setup(enableRear,GPIO.OUT)
+GPIO.setup(MOTOR1_input1,GPIO.OUT)
+GPIO.setup(MOTOR1_input2,GPIO.OUT)
+GPIO.setup(MOTOR2_input1,GPIO.OUT)
+GPIO.setup(MOTOR2_input2,GPIO.OUT)
+GPIO.setup(MOTOR3_input1,GPIO.OUT)
+GPIO.setup(MOTOR3_input2,GPIO.OUT)
+GPIO.setup(MOTOR4_input1,GPIO.OUT)
+GPIO.setup(MOTOR4_input2,GPIO.OUT)
 
-GPIO.output(input1,GPIO.HIGH)
-GPIO.output(input2,GPIO.LOW)
+#Initial State
+off(MOTOR1_input1,MOTOR1_input2,MOTOR2_input1,MOTOR2_input2,MOTOR3_input1,MOTOR3_input2,MOTOR4_input1,MOTOR4_input2)
 
-enablePWM.ChangeDutyCycle(50)
-
-time.sleep(3)
-enablePWM.ChangeDutyCycle(100)
-time.sleep(3)
-enablePWM.ChangeDutyCycle(0)
+#Create PWM instance with frequency of 50 Hz
+#Start PWM with a duty cycle of 0
+PWM_enableFront = GPIO.PWM(enableFront,50)
+PWM_enableRear = GPIO.PWM(enableRear,50)
+PWM_enableFront.start(0)
+PWM_enableRear.start(0)
