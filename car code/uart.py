016981d8be71038de50b1676ce9d437d0da6c219
Joe <kuttehj@etown.edu>
2023-02-27 11:16:04 -0500

    comments

--- car code/uart.py
+++ car code/uart.py
@@ -1,14 +1,15 @@
 import serial
 import time
 
-#Creates serial object for Arduino at a baud rate of 9600 
+#Creates serial object for Arduino at a baud rate of 9600
+#It is imortant that baud rates are equal or data transmission cannot occur
 arduino = serial.Serial('/dev/ttyTHS1',9600,timeout=1)
 
-#Loop to read in data whenever it is avaliable
+#Loop to poll recieved data
 while True:
 	if arduino.in_waiting > 0:
 
-		#Reading in line of data
+		#Reading in line of datas
 		data = arduino.readline().strip()
 		try:
 			#Casting it to an integer and printing
